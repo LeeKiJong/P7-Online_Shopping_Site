e9d09b055894ebcc34acdf91a4266e6f0fb52b77
kijong0822@gmail.com <kijong0822@gmail.com>
2020-08-20 16:50:44 +0900

    비밀번호 암호화 끝

--- src/main/java/com/shopping/controller/MemberController.java
+++ src/main/java/com/shopping/controller/MemberController.java
@@ -4,10 +4,10 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.shopping.domain.MemberVO;
@@ -22,8 +22,8 @@ public class MemberController {
 	@Inject
 	MemberService service;
 	
-	/*@Autowired
-	BCryptPasswordEncoder passEncoder;*/
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
 	
 	// 회원 가입 GET
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
@@ -36,12 +36,12 @@ public class MemberController {
 	public String postSignup(MemberVO vo) throws Exception {
 	 Logger.info("post signup");
 	  
-	 /*String inputPass = vo.getUserPass();
-	 String pass = passEncoder.encode(inputPass);
-	 vo.setUserPass(pass);*/
-	 
 	 String inputPass = vo.getUserPass();
-	 vo.setUserPass(inputPass);
+	 String pass = passwordEncoder.encode(inputPass);
+	 vo.setUserPass(pass);
+	 
+	 /*String inputPass = vo.getUserPass();
+	 vo.setUserPass(inputPass);*/
 
 	 service.signup(vo);
 
